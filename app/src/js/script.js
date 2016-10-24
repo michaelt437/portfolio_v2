ae3d9ee2e7d0881457f5771f34f557d69e5fa5a8
michaelt437 <michaelt437@gmail.com>
2016-10-24 00:00:09 -0400

    updates

--- app/src/js/script.js
+++ app/src/js/script.js
@@ -1,21 +1,41 @@
 var homepage = document.getElementsByTagName('section');
 var reveal = function() {
-    for(i = 1; i < homepage.length; i++){
-        $(homepage[i]).css({'display': 'block'});
+    for (i = 1; i < homepage.length; i++) {
+        $(homepage[i]).css({
+            'display': 'block'
+        });
     }
 }
 
-    /*************
-    * PARALLAX SCROLL
-    * **********/
 
-    // window.onscroll = function() {
-    //     var shift = -.5;
-    //     var bg = $('.home-bg');
-    //     var yPos = window.pageYOffset;
-    //     var pos = '50%' + (yPos * shift) + 'px';
-    //     bg.css({ 'background-position': pos });
-    // }
 
-var tl = new TimelineMax({repeat:10, repeatDelay: 1});
-tl.to($('.home-bg'), 60, {backgroundSize: "200%"})
+
+/*************
+ * PARALLAX SCROLL
+ * **********/
+
+window.onscroll = function() {
+    var top = window.pageYOffset;
+    var homeHeight = document.getElementById('home').scrollHeight;
+    var diff = homeHeight - top;
+    var shift = -.5;
+    var bg = $('.home-bg');
+    var yPos = window.pageYOffset;
+    var pos = '50%' + (yPos * shift) + 'px';
+    bg.css({
+        'background-position': pos
+    });
+    if(diff <= 100){
+        $('.fixed-nav').css({'opacity':1});
+    }else{
+        $('.fixed-nav').css({'opacity':0});
+    }
+}
+
+
+
+
+var tl = new TimelineMax({
+    repeat: 1
+});
+// tl.to($('.home-bg'), 60, {backgroundSize: "200%"});
