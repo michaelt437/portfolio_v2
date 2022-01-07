e3d99722c358806ee6c9bbc980247c63437400ba
Michael Tran <michaelt437@gmail.com>
2022-01-07 00:32:02 -0500

    messing with stuff

--- src/main.js
+++ src/main.js
@@ -2,3 +2,8 @@ document.querySelector("#app").innerHTML = `
   <h1>Hello Vite!</h1>
   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
 `;
+
+document.addEventListener("mousemove", (e) => {
+  // document.documentElement.style.setProperty("--hue", e.clientX * 0.09);
+  document.documentElement.style.setProperty("--degrees", e.clientX + "deg");
+});
