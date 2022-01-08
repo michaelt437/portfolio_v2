deb6921d7c4c1b32e6016c9f9a208cb34cb37e1c
Michael Tran <michaelt437@gmail.com>
2022-01-08 14:44:23 -0500

    mobile navigation: icon, module, css. Closes #6

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,10 +1,6 @@
-import { clickNavItem } from "./modules/navigation.js";
+import { initNavScript } from "./modules/navigation.js";
 
-const navItems = document.querySelectorAll(".nav-item");
-navItems.forEach((item) => {
-  item.addEventListener("click", clickNavItem);
-});
-
+initNavScript();
 document.addEventListener("mousemove", (e) => {
   document.querySelector(":root").style.setProperty("--hue", e.clientX * 0.09);
 });
