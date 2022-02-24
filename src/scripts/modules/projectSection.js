14834a5a933f4017744ffca4fe238b99b208e7ba
Michael Tran <michaelt437@gmail.com>
2022-02-24 00:07:41 -0500

    more progress - comiclib, badges, text

--- src/scripts/modules/projectSection.js
+++ src/scripts/modules/projectSection.js
@@ -1,46 +1,71 @@
 const projectSectionId = document.querySelector("#projects");
 
 const GITHUBHOST = "https://github.com/michaelt437";
+
 const projectsBin = [
   {
     name: "Movie Nights Roulette",
-    description: "",
+    description:
+      "Using data from TMDB, I built this application to easily add movies to a pool and let fate decide tonight's movies with the press of a button.'",
     siteUrl: "https://movienights.netlify.app",
     githubUrl: `${GITHUBHOST}/movie-nights-pwa`,
+    imagePath: "../../img/mnhome.png",
+    stack: ["vue", "typescript", "tailwindcss", "firebase"],
   },
   {
     name: "Comics Library",
     description: "",
-    siteUrl: "https://comiclib.netlify.app",
-    githubtUrl: `${GITHUBHOST}/comiclib-next`,
+    siteUrl: "https://comicslib.netlify.app",
+    githubUrl: `${GITHUBHOST}/comiclib-next`,
+    imagePath: "../../img/clhome.png",
+    stack: ["nextjs", "typescript", "tailwindcss", "supabase"],
   },
   {
     name: "Portfolio",
     description: "",
     siteUrl: "/",
-    githubHost: `${GITHUBHOST}/portfolio_v2`,
+    githubUrl: `${GITHUBHOST}/portfolio_v2`,
+    imagePath: "",
+    stack: ["vite", "javascript"],
   },
 ];
 
 function generateProjectBlocks() {
   return `
-		${projectsBin.map(
-      ({ name, description, siteUrl, githubUrl }) => `
+		${projectsBin
+      .map(
+        ({ name, description, siteUrl, githubUrl, imagePath, stack }) => `
 <div class="section">
 	<div class="details">
 		<h3>${name}</h3>
 		<p class="description">
 			${description}
 		</p>
+		<div class="badge-group">
+			${generateStackBadges(stack)}
+		</div>
 		<div class="btn-group">
-			<a href="${siteUrl}" class="btn btn-tert">Visit Site</a>
-			<a href="${githubUrl}" class="btn">Github</a>
+			<a href="${siteUrl}" target="_blank" rel="noreferrer" class="btn btn-tert">Visit Site</a>
+			<a href="${githubUrl}" target="_blank" rel="noreferrer" class="btn">Github</a>
 		</div>
 	</div>
-	<div class="gallery"><img src="" /></div>
+	<div class="gallery"><a href="${siteUrl}" target="_blank" rel="noreferrer"><img src="${imagePath}" /></a></div>
 </div>
 `
-    )}`;
+      )
+      .join("")}`;
+}
+
+function generateStackBadges(stackArr) {
+  return `
+		${stackArr
+      .map(
+        (tech) => `
+			<div class="badge">${tech}</div>
+		`
+      )
+      .join("")}
+	`;
 }
 
 export function initProjects() {
