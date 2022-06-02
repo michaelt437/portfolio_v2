3e3296b92c26665d6ad3c537325051283eca02f3
Michael Tran <michaelt437@gmail.com>
2022-06-02 09:40:23 -0400

    saljs: install and init

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,4 @@
+import sal from "sal.js";
 import { initNavScript } from "./modules/navigation.js";
 import { initProjects } from "./modules/projectSection.js";
 import { initFooter } from "./modules/footer.js";
@@ -5,6 +6,7 @@ import { initFooter } from "./modules/footer.js";
 initNavScript();
 initProjects();
 initFooter();
+sal();
 //document.addEventListener("mousemove", (e) => {
 //  document.querySelector(":root").style.setProperty("--hue", e.clientX * 0.09);
 //});
