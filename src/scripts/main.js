0d66c2ace2147179a96f8e1e959164d8476f9054
Michael Tran <michaelt437@gmail.com>
2022-06-19 10:45:30 -0400

    import resume in main

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,6 +2,7 @@ import sal from "sal.js";
 import { initNavScript } from "./modules/navigation.js";
 import { initProjects } from "./modules/projectSection.js";
 import { initFooter } from "./modules/footer.js";
+import resume from "../resume.pdf";
 
 initNavScript();
 initProjects();
@@ -10,6 +11,10 @@ sal();
 //document.addEventListener("mousemove", (e) => {
 //  document.querySelector(":root").style.setProperty("--hue", e.clientX * 0.09);
 //});
+
+document.querySelector("#resume").addEventListener("click", () => {
+  window.open(resume);
+});
 
 window.scrollToProjects = () => {
   document.getElementById("projects-anchor").scrollIntoView({
